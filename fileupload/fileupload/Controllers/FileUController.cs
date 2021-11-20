abe9b7a6361a1166b632567f42289f7257bb7df8
William Diaz <williamefrain1994@gmail.com>
2021-11-20 15:48:49 -0600

    go

---
+++ fileupload/fileupload/Controllers/FileUController.cs
@@ -0,0 +1,40 @@
+﻿using fileupload.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fileupload.Controllers
+{
+    public class FileUController : Controller
+    {
+        // GET: FileU
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Imagen imagen )
+        {
+
+            _ = imagen.file;
+
+
+
+            String ruta = Server.MapPath("~/img/");
+            ruta += imagen.file.FileName;
+
+            imagen.file.SaveAs(ruta);
+
+            return View();
+        }
+
+        public ActionResult Second() {
+
+            return View();
+        }
+    }
+}
---
+++ fileupload/fileupload/Models/Imagen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fileupload.Models
+{
+    public class Imagen
+    {
+        public HttpPostedFileBase file { get; set; }
+    }
+}
